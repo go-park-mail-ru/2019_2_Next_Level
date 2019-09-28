ddbe901055e06255a580e4d53f141c75b94cb454
Ian <andreykochnov@yandex.ru>
2019-09-28 18:53:49 +0300

    Маршруты gorilla/mux

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -4,11 +4,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"regexp"
 	"strconv"
 	"time"
+
+	"github.com/gorilla/mux"
 
 	uuid "github.com/google/uuid"
 
@@ -24,6 +25,132 @@ type Myerror struct {
 }
 
 var config Config
+
+type AuthHandler struct {
+}
+
+func (a *AuthHandler) Login(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("Cannot get body")
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	user := UserInput{}
+	if err := json.Unmarshal(body, &user); err != nil {
+		fmt.Println("Error during parse profile", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	dbUser, err := db.GetUserByEmail(user.Email)
+	if err != nil {
+		fmt.Println("No such a user")
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if dbUser.Password != user.Password {
+		fmt.Println("Wrong pasword")
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	fmt.Println("Right user")
+	out, _ := uuid.NewUUID()
+	db.RegisterNewSession(out.String(), user.Email)
+	cookie := http.Cookie{
+		Name:    "user-id",
+		Value:   out.String(),
+		Expires: time.Now().Add(10 * time.Hour),
+	}
+	r.AddCookie(&cookie)
+	http.SetCookie(w, &cookie)
+}
+
+func (a *AuthHandler) Register(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("Cannot get body")
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	user := UserInput{}
+	if err := json.Unmarshal(body, &user); err != nil {
+		fmt.Println("Error during parse profile", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Println("User")
+	db.SetUser(user.ToUser())
+	out, _ := uuid.NewUUID()
+	db.RegisterNewSession(out.String(), user.Email)
+	cookie := http.Cookie{
+		Name:    "user-id",
+		Value:   out.String(),
+		Expires: time.Now().Add(10 * time.Hour),
+	}
+	http.SetCookie(w, &cookie)
+	w.WriteHeader(http.StatusOK)
+}
+
+type DataHandler struct {
+}
+
+func (h *DataHandler) GetProfile(w http.ResponseWriter, r *http.Request) {
+	status := http.StatusBadRequest
+
+	session, err := r.Cookie("user-id")
+	if err != nil {
+		fmt.Println("Unauthorized user")
+		w.WriteHeader(status)
+		return
+	}
+	fmt.Printf("Cookie: %s\n", session.Value)
+	email, err := db.GetUserEmailBySession(session.Value)
+	if err != nil {
+		fmt.Println("Unauthorized user")
+		w.WriteHeader(status)
+		return
+	}
+	user, err := db.GetUserByEmail(email)
+	if err != nil {
+		fmt.Printf("Cannot get user: %s\n", err)
+		w.WriteHeader(status)
+		return
+	}
+	fmt.Println(user)
+	js, err := json.Marshal(user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+	status = http.StatusAccepted
+}
+
+func (h *DataHandler) GetFront(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	if path == "/" {
+		path = "/index.html"
+	}
+	fmt.Printf("Path: %s\n", r.URL.Path)
+	path = config.FrontendPath + path
+	file, err := ioutil.ReadFile(path)
+	if err != nil {
+		fmt.Printf("Cannot open file %s: %s\n", path, err)
+		return
+	}
+	fmt.Printf("Give %s file\n", path)
+
+	contentType := getFileType(path)
+	w.Header().Set("Content-Type", contentType)
+	w.Write(file)
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
@@ -185,25 +312,17 @@ func getFileType(filename string) string {
 func Run(cfg *Config) error {
 	config = *cfg
 	fmt.Printf("Starting daemon on port %d\n", cfg.Port)
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", handler)
-	mux.HandleFunc("/profile", getProfileHandler)
-	mux.HandleFunc("/signup", registrateUserHandler)
-	mux.HandleFunc("/signin", authorizeUserHandler)
-
-	server := http.Server{
-		Addr:         ":" + strconv.Itoa(cfg.Port),
-		Handler:      mux,
-		ReadTimeout:  10 * time.Millisecond,
-		WriteTimeout: 10 * time.Millisecond,
-	}
 	db.Init()
-	server.ListenAndServe()
-	if err := server.ListenAndServe(); err != nil {
-		log.Printf("Cannot start listening port %d", cfg.Port)
-		return err
-	}
+
+	router := mux.NewRouter()
+	authApi := &AuthHandler{}
+	dataApi := &DataHandler{}
+	router.HandleFunc("/signup", authApi.Register)
+	router.HandleFunc("/signin", authApi.Login)
+	router.HandleFunc("/profile", dataApi.GetProfile)
+	router.PathPrefix("/").HandlerFunc(dataApi.GetFront)
+
+	http.ListenAndServe(":"+strconv.Itoa(cfg.Port), router)
 
 	return nil
 }
