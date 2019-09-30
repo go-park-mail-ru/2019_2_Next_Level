0e1a39e3c30b5751f642a6b6ee523853668aacad
Ian <andreykochnov@yandex.ru>
2019-09-30 16:50:37 +0300

    change address

--- daemon/CorsHandler.go
+++ daemon/CorsHandler.go
@@ -10,9 +10,9 @@ type CorsHandler struct {
 
 func (h *CorsHandler) preflightHandler(w http.ResponseWriter, r *http.Request) {
 	whitelist := map[string]bool{
-		config.FrontendUrl:                               true,
-		"https://next-level-mail.kerimovdev.now.sh/":     true,
-		"https://next-level-mail.ivanovvanya111.now.sh/": true,
+		config.FrontendUrl:                              true,
+		"https://next-level-mail.kerimovdev.now.sh/":    true,
+		"https://next-level-mail.ivanovvanya111.now.sh": true,
 	}
 	headers := w.Header()
 	// headers.Add("Access-Control-Allow-Origin", config.FrontendUrl)
@@ -25,6 +25,8 @@ func (h *CorsHandler) preflightHandler(w http.ResponseWriter, r *http.Request) {
 	if whitelist[origin] {
 		headers.Add("Access-Control-Allow-Origin", origin)
 		log.Println("In whitelist")
+	} else {
+		log.Println("Not in whitelist: ", origin)
 	}
 	headers.Add("Access-Control-Allow-Credentials", "true")
 	headers.Add("Access-Control-Allow-Headers", "Content-Type")
