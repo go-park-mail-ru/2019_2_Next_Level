660bbc9ec926253d8d2e386ae688a1283db24ce4
Ian <andreykochnov@yandex.ru>
2019-11-06 11:17:07 +0300

    Sanitizer for mail

--- internal/serverapi/server/MailBox/usecase/usecase.go
+++ internal/serverapi/server/MailBox/usecase/usecase.go
@@ -8,14 +8,17 @@ import (
 	"2019_2_Next_Level/internal/serverapi/config"
 	e "2019_2_Next_Level/internal/serverapi/server/Error"
 	mailbox "2019_2_Next_Level/internal/serverapi/server/MailBox"
+	"github.com/microcosm-cc/bluemonday"
 )
 
 type MailBoxUsecase struct {
 	repo mailbox.MailRepository
 	smtpPort postinterface.IPostInterface
 }
+var sanitizer *bluemonday.Policy
 
 func NewMailBoxUsecase(repo mailbox.MailRepository) *MailBoxUsecase {
+	sanitizer = bluemonday.UGCPolicy()
 	usecase := MailBoxUsecase{repo: repo}
 	usecase.smtpPort = postinterface.NewQueueClient(config.Conf.HttpConfig.PostServiceHost, config.Conf.HttpConfig.PostServiceSendPort)
 	usecase.smtpPort.Init()
@@ -23,7 +26,11 @@ func NewMailBoxUsecase(repo mailbox.MailRepository) *MailBoxUsecase {
 }
 
 func (u *MailBoxUsecase) SendMail(from, to, body string) error {
-	email := post.Email{From: from, To: to, Body: body}
+	email := post.Email{
+		From: sanitizer.Sanitize(from),
+		To: sanitizer.Sanitize(to),
+		Body: sanitizer.Sanitize(body),
+	}
 	err := u.smtpPort.Put(email)
 	log.Log().I("Send mail:\n From: %s\n To: %s\nBody: %s\n", from, to, body)
 	log.Log().I(err)
@@ -34,6 +41,11 @@ func (u *MailBoxUsecase) GetMailList(login string, folder string, sort string, f
 	list, err := u.repo.GetEmailList(login, folder, sort, from, count)
 	if err != nil {
 		return list, e.Error{}.SetError(err).SetCode(e.ProcessError)
+	}
+	for i := range list {
+		list[i].From = sanitizer.Sanitize(list[i].From)
+		list[i].To = sanitizer.Sanitize(list[i].To)
+		list[i].Body = sanitizer.Sanitize(list[i].Body)
 	}
 	return list, nil
 }
