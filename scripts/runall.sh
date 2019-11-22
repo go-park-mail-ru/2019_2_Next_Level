c6753c4dfd9efb0305ba7e7c87162bfce700338d
Ian <ivanov.vanya.111@mail.ru>
2019-11-22 18:21:24 +0000

    build

--- scripts/runall.sh
+++ scripts/runall.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-go run ../cmd/MailPicker/main.go -config=../config/mailpicker.config.json -dbuser go -dbpass postgres &
+../build/auth_service &
 
-go run ../cmd/serverapi/main.go -config=../config/http_service.config.json -dbuser go -dbpass postgres &
+#go run ../cmd/MailPicker/main.go -config=../config/mailpicker.config.json -dbuser go -dbpass postgres &
 
-go run ../cmd/post/main.go -config=../config/post_service.config.json &
+../build/http_service -config=../config/http_service.config.json -dbuser go -dbpass postgres &
+
+#go run ../cmd/post/main.go -config=../config/post_service.config.json &
 # chmod +x ./runall.sh
