851b277226611a7aee3d0efacacc0ff77c5bfff1
Ian <andreykochnov@yandex.ru>
2019-12-15 15:49:30 +0300

    change runall

--- scripts/runall.travis.sh
+++ scripts/runall.travis.sh
@@ -6,7 +6,7 @@ kill $(pgrep ../build -f)
 
 ../mailpicker_service -config=../config/mailpicker.config.json -dbuser go -dbpass postgres &
 
-..g/http_service -config=../config/http_service.config.json -dbuser go -dbpass postgres &
+../http_service -config=../config/http_service.config.json -dbuser go -dbpass postgres &
 
 #go run ../cmd/post/main.go -config=../config/post_service.config.json &
 # chmod +x ./runall.sh
