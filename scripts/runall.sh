22e7ee12c68ec0a268008f8493779cb3a8854849
Ian <andreykochnov@yandex.ru>
2019-12-15 17:56:11 +0300

    FUUCK

--- scripts/runall.sh
+++ scripts/runall.sh
@@ -7,7 +7,7 @@ kill $(pgrep ../build -f)
 
 ../build/mailpicker_service -config=../config/mailpicker.config.json -dbuser go -dbpass postgres &
 
-../build/http_service -config=../config/http_service.config.json -dbuser go -dbpass postgres &
+../build/http_service -config=../config/linux_http_service.config.json -dbuser go -dbpass postgres &
 
 #go run ../cmd/post/main.go -config=../config/post_service.config.json &
 # chmod +x ./runall.sh
