ea863cf120c3377fca0b180c877c2d02b0bc4282
Ian <andreykochnov@yandex.ru>
2019-12-15 15:43:08 +0300

    d

---
+++ scripts/runall.travis.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+kill $(pgrep ../build -f)
+
+../auth_service &
+
+
+../mailpicker_service -config=../config/mailpicker.config.json -dbuser go -dbpass postgres &
+
+..g/http_service -config=../config/http_service.config.json -dbuser go -dbpass postgres &
+
+#go run ../cmd/post/main.go -config=../config/post_service.config.json &
+# chmod +x ./runall.sh
