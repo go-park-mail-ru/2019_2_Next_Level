bef3eaab100d8831722a6a244ddc9aba7553cbdc
Ian <andreykochnov@yandex.ru>
2019-12-15 16:02:02 +0300

    d

--- scripts/runall.travis.sh
+++ scripts/runall.travis.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 kill $(pgrep auth_service -f)
 kill $(pgrep mailpicker_service -f)
-kill $(pgrep http_serviceg -f)
+kill $(pgrep http_service -f)
 
 ../auth_service &
 
 
 ../mailpicker_service -config=../config/mailpicker.config.json -dbuser go -dbpass postgres &
 
-../http_service -config=../config/http_service.config.json -dbuser go -dbpass postgres &
+../http_service -config=../config/linux_http_service.config.json -dbuser go -dbpass postgres &
 
 #go run ../cmd/post/main.go -config=../config/post_service.config.json &
 # chmod +x ./runall.sh
